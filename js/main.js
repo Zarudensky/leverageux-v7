1d7420a71ff49fd87687f1a7adf9d116cdb09ff6
Stas Zarudensky <szarudensky@gmail.com>
2023-06-20 03:25:20 +0300

    Fix header, footer, form and scripts

--- js/main.js
+++ js/main.js
@@ -90,4 +90,196 @@ $(document).ready(function(){
 		$('.feedback__message-send').hide();
 		$('.feedback__message-notsend').hide();
 	});
+
+	// send form footer and validation
+	function validation() {
+		let valid = true;
+		$('.validation__footer-form').each(function() {
+			if(!$(this).val().length >= 1){
+				$(this).addClass('invalid');
+				validText();
+				valid = false;
+				var errorInput = $(this).attr('name');
+				var eventActionInput = 'validation error - ' + errorInput;
+				dataLayer.push({
+					'eventCategory': 'form',
+					'eventAction': eventActionInput,
+					'eventLabel': 'footer', 
+					'event': 'autoEvent',
+				});
+			}
+		});
+		if( !validEmail($('.validation__footer-email').val())) {
+			$('.validation__footer-email').addClass('invalid');
+			$('.validation__text-footer').addClass('active');
+			valid = false;
+		}
+		return valid;
+	}
+	function validEmail(email) {
+		var regexEmail = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+		return regexEmail.test(email);
+	}
+	function validText() {
+		$('.validation__footer-form').each(function() {
+			if(!$(this).val().length >= 1){
+				$('.validation__text-footer').addClass('active');
+			} else {
+				$('.validation__text-footer').removeClass('active');
+			}
+		});
+	}
+	$('.validation__footer-form').focus(function() {
+		$(this).removeClass('invalid');
+	});
+	$('.validation__textarea').keyup(function() {
+		validText();
+	});
+	$('#formBtnFooter').click(function() {
+		if (validation()) {
+			sendForm()
+		}
+	});
+
+	function sendForm() {
+		var firstname = $('#nameFooter').val();
+		var email = $('#emailFooter').val();
+		var descr = $('#descrFooter').val();
+
+		var feedbackModal = $('#feedbackModal');
+		var feedbackMessageSend = $('.feedback__message-send');
+		var feedbackMessageNotSend = $('.feedback__message-notsend');
+
+		var leadStatus = 'Interest';
+		var contactType = 'Prospect';
+		var source = 'Website';
+		var leadGeneration = 'Inbound';
+
+		var xhr = new XMLHttpRequest();
+		var url = 'https://api.hsforms.com/submissions/v3/integration/submit/6484354/ba6957be-53f3-4cc6-877f-d0dc0d677081'
+		// Example request JSON:
+		var data = {
+			"fields": [
+				{
+					"name": "firstname",
+					"value": firstname
+				},
+				{
+					"name": "email",
+					"value": email
+				},
+				{
+					"name": "description",
+					"value": descr
+				},
+				{
+					"name": "hs_lead_status",
+					"value": leadStatus
+				},
+				{
+					"name": "contact_type",
+					"value": contactType
+				},
+				{
+					"name": "source",
+					"value": source
+				},
+				{
+					"name": "lead_generation",
+					"value": leadGeneration
+				}
+			]
+		}
+		var final_data = JSON.stringify(data)
+		xhr.open('POST', url);
+		// Sets the value of the 'Content-Type' HTTP request headers to 'application/json'
+		xhr.setRequestHeader('Content-Type', 'application/json');
+		xhr.onreadystatechange = function() {
+			$('#nameFooter').val('');
+			$('#emailFooter').val('');
+			$('#descrFooter').val('');
+			$('.validation__text').removeClass('active');
+
+			if(xhr.readyState == 4 && xhr.status == 200) { 
+				$(feedbackMessageNotSend).hide();
+				$(feedbackMessageSend).show();
+				$(feedbackModal).show();
+				dataLayer.push({
+					'eventCategory': 'form',
+					'eventAction': 'sent',
+					'eventLabel': 'footer', 
+					'event': 'autoEvent',
+				});				
+			} else if (xhr.readyState == 4 && xhr.status == 400){
+				$(feedbackMessageSend).hide();
+				$(feedbackMessageNotSend).show();
+				$(feedbackModal).show();
+				console.log(xhr.responseText);
+				var error = JSON.parse(xhr.responseText);
+				var errorText = error.errors[0].errorType;
+				var eventActionText = 'error - ' + errorText;
+				dataLayer.push({
+					'eventCategory': 'form',
+					'eventAction': eventActionText,
+					'eventLabel': 'footer', 
+					'event': 'autoEvent',
+				});				
+			} else if (xhr.readyState == 4 && xhr.status == 403){ 
+				$(feedbackMessageSend).hide();
+				$(feedbackMessageNotSend).show();
+				$(feedbackModal).show();
+				console.log(xhr.responseText);
+				var error = JSON.parse(xhr.responseText);
+				var errorText = error.errors[0].errorType;
+				var eventActionText = 'error - ' + errorText;
+				dataLayer.push({
+					'eventCategory': 'form',
+					'eventAction': eventActionText,
+					'eventLabel': 'footer', 
+					'event': 'autoEvent',
+				});
+			} else if (xhr.readyState == 4 && xhr.status == 404){ 
+				$(feedbackMessageSend).hide();
+				$(feedbackMessageNotSend).show();
+				$(feedbackModal).show();
+				console.log(xhr.responseText);
+				var error = JSON.parse(xhr.responseText);
+				var errorText = error.errors[0].errorType;
+				var eventActionText = 'error - ' + errorText;
+				dataLayer.push({
+					'eventCategory': 'form',
+					'eventAction': eventActionText,
+					'eventLabel': 'footer', 
+					'event': 'autoEvent',
+				});
+			}
+
+			function hideModalAndMessages(){
+				$(feedbackModal).hide();
+				$(feedbackMessageNotSend).hide();
+				$(feedbackMessageSend).hide();
+			}
+			setTimeout(hideModalAndMessages, 3000);
+		} 
+		// Sends the request	    
+		xhr.send(final_data)	
+	}
+	$('.close, .modal__back').click(function() {
+		$('#feedbackModal').hide();
+		$('.feedback__message-send').hide();
+		$('.feedback__message-notsend').hide();
+	});
+});
+	
+// copied to clipboard email
+document.querySelector('#emailCopy').addEventListener('click', () => {
+	navigator.clipboard.writeText('hello@leverageux.com')
+	.then(() => {
+			let message = document.getElementById('copyMessage');
+			message.style.display = "block";
+			setTimeout(() => message.style.display = "none", 2000);
+	})
+	.catch(err => {
+			console.log('Failed to copy email to clipboard', err);
+	});
 });
